a3582e383704f5cd8b502492f463993975bb4ab2
marianorigo18 <marianowagnerrigo@gmail.com>
2023-02-20 23:02:43 -0300

    first commit

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const boxContent = document.querySelector('.box-container');
 const containerModal = document.querySelector('.container-modal');
+const containerModalCalculator = document.querySelector('.container-modal-calculator');
 
 boxContent.addEventListener('click', (e)=> {
     const cuadrado = e.target.classList.contains('cuadrado');
@@ -12,8 +13,21 @@ containerModal.addEventListener('click', (e) => {
     const btnClose = e.target.classList.contains('btn-close');
     const fueraDelModal = e.target.classList.contains('container-modal');
     const btnCloseTwo = e.target.classList.contains('btn-close-two');
-
+    /*other modal*/
+    const btnOpenOthelModal = e.target.classList.contains('btn-open-other-modal');
     if(btnClose || fueraDelModal || btnCloseTwo){
-        containerModal.classList.remove('open-modal')
+        return containerModal.classList.remove('open-modal')
+    }
+    if(btnOpenOthelModal){
+        containerModalCalculator.classList.add('open-modal-calculator')
+    }
+})
+
+containerModalCalculator.addEventListener('click', (e) => {
+    const btnCloseModal = e.target.classList.contains('btn-close-calculator');
+    const btnCloseModalTwo = e.target.classList.contains('btn-close-two-calculator');
+    const fueraDelModalTwo = e.target.classList.contains('container-modal-calculator');
+    if(btnCloseModalTwo || btnCloseModal || fueraDelModalTwo){
+        containerModalCalculator.classList.remove('open-modal-calculator')
     }
 })
